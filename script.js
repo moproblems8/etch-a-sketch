25388c083c1c5bfa419160dae593e1c52e3bf382
moproblems8 <mhasan1080@gmail.com>
2023-06-28 20:10:15 -0700

    slider works (but)
    as i keep changing the slider, the grid box gets messed up. it keeps expanding and the grid items disappear eventaully

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.getElementById("container");
+const rangeSliderValue = document.getElementById("range-slider");
+const rangeSliderTextOutput = document.getElementById("range-value");
 
-function makeRows(rows, cols) {
+
+function makeGrid(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
@@ -9,4 +12,14 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(8, 8);
+makeGrid(8, 8);
+
+// range slide
+
+rangeSliderValue.oninput = function() {
+
+    const sliderValue = this.value;
+    rangeSliderTextOutput.innerHTML = sliderValue + 'x' + sliderValue;
+
+    makeGrid(sliderValue, sliderValue);
+};
