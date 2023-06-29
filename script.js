155f0876a7528fda6f133694a099c7c2ec975cd0
moproblems8 <mhasan1080@gmail.com>
2023-06-28 20:13:45 -0700

    GRID BOX WORKS!

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ const rangeSliderTextOutput = document.getElementById("range-value");
 
 
 function makeGrid(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "grid-item";
-  };
+    container.innerHTML = "";
+
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-item";
+    };
 };
 
 makeGrid(8, 8);
